7147ff69b801a94542962685c61d42794efd1896
tim <timofeykasmolin@gmail.com>
2022-05-21 22:02:16 +0500

    Из проекта удален весь незаконный контент)

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+<head>
+    <title>Ошибка 404</title>
+</head>
+<body>
+<img src='https://avatars.mds.yandex.net/get-zen_doc/196516/pub_5ade6b88610493953ec1d802_5ade6ba6d7bf21a6c9ee9363/scale_1200' width="100%" height="100%">
+
+</body>
+
+
